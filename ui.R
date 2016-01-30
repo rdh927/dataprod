17a2bd9da08714c12fefb927e0e79f5dbbd29ce8
mc1r_variant <rachel.dory.harris@gmail.com>
2016-01-30 17:07:10 -0600

    Update ui.R

--- ui.R
+++ ui.R
@@ -22,11 +22,14 @@ shinyUI(
     verbatimTextOutput("documentation")
   ),
   mainPanel(
-    h4('You entered'),
-    verbatimTextOutput("inputValue"),
-    h4('The number of compounds in that fruit is: '),
-    verbatimTextOutput("comp_number"),
-    h4('This fruit has the following compounds and health effects: '),
-    tableOutput("prediction")
+    tabsetPanel(
+      tabPanel("Documentation", verbatimTextOutput("documentation")), 
+      tabPanel("Results", h4('You entered'),
+               verbatimTextOutput("inputValue"),
+               h4('The number of compounds in that fruit is: '),
+               verbatimTextOutput("comp_number"),
+               h4('This fruit has the following compounds and health effects: '),
+               tableOutput("prediction"))
+    
   )
-))
+)))
