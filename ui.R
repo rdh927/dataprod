3a751e5adc425cdf71fa7d525f48f54325e249e2
mc1r_variant <rachel.dory.harris@gmail.com>
2016-01-30 17:07:33 -0600

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,11 +1,6 @@
 #Your Shiny Application
 
 #Write a shiny application with associated supporting documentation. 
-#The documentation should be thought of as whatever a user will need to get started using your application.
-#Deploy the application on Rstudio's shiny server
-#Share the application link by pasting it into the text box below
-#Share your server.R and ui.R code on github
-#The application must include the following:
 
 
 #The documentation should be at the Shiny website itself. Do not post to an external link.
